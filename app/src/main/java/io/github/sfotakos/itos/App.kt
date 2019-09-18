53f8edabd22a5c5a2474aecf99839e514eede8e6
SFotakos <s.fotakos@gmail.com>
2019-09-18 19:32:03 -0300

    Changes folder structure to better suit clean pattern

---
+++ app/src/main/java/io/github/sfotakos/itos/App.kt
@@ -0,0 +1,6 @@
+package io.github.sfotakos.itos
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/io/github/sfotakos/itos/data/entities/APOD.kt
@@ -0,0 +1,11 @@
+package io.github.sfotakos.itos.data.entities
+
+
+data class APOD (
+    val date : String,
+    val explanation : String,
+    val hdurl : String,
+    val media_type : String,
+    val service_version : String,
+    val title : String,
+    val url : String)
