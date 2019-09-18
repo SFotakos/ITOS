44c4f7dfe58765ab4f1d1f635faa23bc770cbe8f
SFotakos <s.fotakos@gmail.com>
2019-09-18 19:49:53 -0300

    Adds API json mapping to APOD data class

--- app/src/main/java/io/github/sfotakos/itos/data/entities/APOD.kt
+++ app/src/main/java/io/github/sfotakos/itos/data/entities/APOD.kt
@@ -1,11 +1,13 @@
 package io.github.sfotakos.itos.data.entities
 
+import com.google.gson.annotations.SerializedName
 
 data class APOD (
-    val date : String,
-    val explanation : String,
-    val hdurl : String,
-    val media_type : String,
-    val service_version : String,
-    val title : String,
-    val url : String)
+    @SerializedName("copyright") val copyright : String,
+    @SerializedName("date") val date : String,
+    @SerializedName("explanation") val explanation : String,
+    @SerializedName("hdurl") val hdurl : String,
+    @SerializedName("media_type") val media_type : String,
+    @SerializedName("service_version") val service_version : String,
+    @SerializedName("title") val title : String,
+    @SerializedName("url") val url : String)
