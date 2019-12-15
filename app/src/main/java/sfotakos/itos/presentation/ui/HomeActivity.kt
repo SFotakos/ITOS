0e799860b2ff11ce5dc0cedc9e03d696a02eb181
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2019-12-15 12:40:07 -0300

    Improves loading background color

--- app/src/main/java/sfotakos/itos/presentation/ui/HomeActivity.kt
+++ app/src/main/java/sfotakos/itos/presentation/ui/HomeActivity.kt
@@ -3,7 +3,6 @@ package sfotakos.itos.presentation.ui
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
